88049d58f06daa2c5fb70f83c6d8cd2d5486606e
tnals2384 <tnals2384@gmail.com>
2024-11-17 20:57:10 +0900

    [ARV-9] chore: Exception & ErrorCode setting

---
+++ src/main/java/com/kwanse/allreva/common/exception/CustomControllerAdvice.java
@@ -0,0 +1,36 @@
+package com.kwanse.allreva.common.exception;
+
+import com.kwanse.allreva.common.dto.Response;
+import com.kwanse.allreva.common.exception.code.GlobalErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class CustomControllerAdvice {
+
+    @ExceptionHandler(value = CustomException.class)
+    public ResponseEntity<?> handleCustomException(CustomException e) {
+        return ResponseEntity.status(e.getErrorCode().status())
+                .body(
+                        Response.onFailure(
+                                e.getErrorCode().code(),
+                                e.getErrorCode().message()
+                        )
+                );
+
+    }
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<?> handleException(Exception e) {
+        log.error("error: {}", e.getMessage(), e);
+        return ResponseEntity.status(GlobalErrorCode.SERVER_ERROR.getErrorCode().status())
+                .body(
+                        Response.onFailure(
+                                GlobalErrorCode.SERVER_ERROR.getErrorCode().code(),
+                                GlobalErrorCode.SERVER_ERROR.getErrorCode().message()
+                               )
+                );
+    }
+}
