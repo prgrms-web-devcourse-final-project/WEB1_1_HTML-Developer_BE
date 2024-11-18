8e51f67798e8fb4bdb2b89a49290cbd99b24d184
tnals2384 <tnals2384@gmail.com>
2024-11-18 12:02:30 +0900

    [ARV-9] chore: ResponseConst 제거

--- src/main/java/com/kwanse/allreva/common/dto/ResponseConst.java
+++
@@ -1,6 +0,0 @@
-package com.kwanse.allreva.common.dto;
-
-public final class ResponseConst {
-    public static final String SUCCESS = "SUCCESS";
-    public static final String SUCCESS_MESSAGE = "요청에 성공하였습니다.";
-}
