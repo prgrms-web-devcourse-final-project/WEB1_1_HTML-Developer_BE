9505a24f8f65c64290aa6f287a8560ca83937754
sangcci <pshsh910@gmail.com>
2024-11-20 12:42:32 +0900

    [ARV-19] refactor: OAuth2UserInfo 타입 경고 억제 작업
    - 카카오 API의 응답 구조가 확실하기 때문에, `@SuppressWarnings`을 통해 억제

---
+++ src/main/java/com/backend/allreva/auth/oauth2/application/dto/OAuth2UserInfo.java
@@ -0,0 +1,42 @@
+package com.backend.allreva.auth.oauth2.application.dto;
+
+import java.util.Map;
+
+import com.backend.allreva.member.command.domain.LoginProvider;
+
+import lombok.Builder;
+
+@Builder
+public record OAuth2UserInfo(
+        LoginProvider loginProvider,
+        String providerId,
+        String nickname,
+        String email,
+        String profile
+) {
+
+    public static OAuth2UserInfo ofGoogle(Map<String, Object> attributes) {
+        return OAuth2UserInfo.builder()
+                .loginProvider(LoginProvider.GOOGLE)
+                .providerId((String) attributes.get("sub"))
+                .nickname((String) attributes.get("nickname"))
+                .email((String) attributes.get("email"))
+                .profile((String) attributes.get("picture"))
+                .build();
+    }
+
+    public static OAuth2UserInfo ofKakao(Map<String, Object> attributes) {
+        @SuppressWarnings("unchecked")
+        Map<String, Object> account = (Map<String, Object>) attributes.get("kakao_account");
+        @SuppressWarnings("unchecked")
+        Map<String, Object> profile = (Map<String, Object>) account.get("profile");
+        Long providerId = (Long) attributes.get("id");
+
+        return OAuth2UserInfo.builder()
+                .loginProvider(LoginProvider.KAKAO)
+                .providerId(String.valueOf(providerId))
+                .nickname((String) profile.get("nickname"))
+                .email((String) account.get("email"))
+                .build();
+    }
+}
