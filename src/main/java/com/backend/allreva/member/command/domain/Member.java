28ef7e284d92d6574fd304299ba0b5e93d6473fc
kwanse <khan67781397@gmail.com>
2024-11-20 13:23:13 +0900

    rename: 패키지명 backend 로 변경

---
+++ src/main/java/com/backend/allreva/member/command/domain/Member.java
@@ -0,0 +1,18 @@
+package com.backend.allreva.member.command.domain;
+
+import com.backend.allreva.common.model.Email;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    private Email email;
+}
--- src/main/java/com/kwanse/allreva/member/infra/MemberRepositoryCustom.java
+++
@@ -1,4 +0,0 @@
-package com.kwanse.allreva.member.infra;
-
-public interface MemberRepositoryCustom {
-}
