092c6a71bb162f68ae1176493c9a34776e6882b5
sangcci <pshsh910@gmail.com>
2024-11-22 15:07:52 +0900

    [ARV-19] OAuth2 회원가입 HTTP Method 수정

--- src/main/java/com/backend/allreva/member/ui/OAuth2RegisterController.java
+++ src/main/java/com/backend/allreva/member/ui/OAuth2RegisterController.java
@@ -1,7 +1,7 @@
 package com.backend.allreva.member.ui;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +20,7 @@ public class OAuth2RegisterController {
 
     private final MemberCommandService memberCommandService;
 
-    @PostMapping("/register")
+    @PatchMapping("/register")
     public ResponseEntity<Void> register(
             @AuthMember Member member,
             @RequestBody MemberOAuth2RegisterRequest memberOAuth2RegisterRequest) {
