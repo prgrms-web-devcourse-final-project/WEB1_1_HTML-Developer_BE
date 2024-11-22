3cd7a083fcbb250e5ca1a4dd5042caf25b913e36
tnals2384 <tnals2384@gmail.com>
2024-11-22 15:31:00 +0900

    [ARV-24] feat: ConcertDiary Entity 생성

---
+++ src/main/java/com/backend/allreva/diary/command/domain/ConcertDiary.java
@@ -0,0 +1,41 @@
+package com.backend.allreva.diary.command.domain;
+
+import com.backend.allreva.common.application.BaseEntity;
+import com.backend.allreva.common.model.Image;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLRestriction("deleted_at is NULL")
+@SQLDelete(sql = "UPDATE concert_diary SET deleted_at = NOW() WHERE id = ?")
+@Entity
+public class ConcertDiary extends BaseEntity {
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private Long concertId;
+
+    @Column(nullable = false)
+    private Date date; //날짜
+
+    private String episode; //회차
+
+    @ElementCollection
+    @CollectionTable(
+            name = "diary_images",
+            joinColumns = @JoinColumn(name = "id")
+    )
+    private List<Image> diaryImages;
+}
