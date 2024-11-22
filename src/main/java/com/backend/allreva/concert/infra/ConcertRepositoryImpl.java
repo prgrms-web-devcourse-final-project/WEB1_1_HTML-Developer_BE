6aae01e07f493fe991cfe71f3a43eabeb2952938
kwanse <khan67781397@gmail.com>
2024-11-22 16:16:45 +0900

    [ARV-12] refactor: concert 상세 조회 리팩토링

--- src/main/java/com/backend/allreva/concert/infra/ConcertRepositoryImpl.java
+++ src/main/java/com/backend/allreva/concert/infra/ConcertRepositoryImpl.java
@@ -2,15 +2,11 @@ package com.backend.allreva.concert.infra;
 
 import com.backend.allreva.concert.query.application.ConcertRepositoryCustom;
 import com.backend.allreva.concert.query.application.dto.ConcertDetail;
-import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Expression;
 import com.querydsl.core.types.Projections;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 import static com.backend.allreva.concert.command.domain.QConcert.concert;
 import static com.backend.allreva.hall.command.domain.QConcertHall.concertHall;
@@ -24,23 +20,11 @@ public class ConcertRepositoryImpl implements ConcertRepositoryCustom {
 
     @Override
     public ConcertDetail findDetailById(Long concertId) {
-
-        Tuple tuple = queryFactory.select(concert, concertHall.hallInfo, concertHall.convenienceInfo, concertHall.location)
+        return queryFactory.select(concertDetailProjection())
                 .from(concert)
                 .join(concertHall).on(concertHall.id.eq(concert.code.hallCode))
                 .where(concert.id.eq(concertId))
                 .fetchFirst();
-
-        if (tuple == null) {
-            throw new IllegalArgumentException("No concert found with id " + concertId);
-        }
-
-        return ConcertDetail.builder()
-                .concert(tuple.get(concert))
-                .concertHallInfo(tuple.get(concertHall.hallInfo))
-                .convenienceInfo(tuple.get(concertHall.convenienceInfo))
-                .location(tuple.get(concertHall.location))
-                .build();
     }
 
     private Expression<ConcertDetail> concertDetailProjection() {
@@ -51,5 +35,5 @@ public class ConcertRepositoryImpl implements ConcertRepositoryCustom {
                 concertHall.location
         );
     }
-
 }
+
