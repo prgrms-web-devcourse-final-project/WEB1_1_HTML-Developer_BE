3b1e29b734ba779a9cc09942b989cbc296ba2483
sangcci <pshsh910@gmail.com>
2024-11-22 12:11:12 +0900

    [ARV-19] chore: @RequestBody import 수정

--- src/main/java/com/backend/allreva/member/ui/OAuth2RegisterController.java
+++ src/main/java/com/backend/allreva/member/ui/OAuth2RegisterController.java
@@ -2,13 +2,13 @@ package com.backend.allreva.member.ui;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.allreva.member.command.application.MemberCommandService;
 import com.backend.allreva.member.command.application.dto.MemberOAuth2RegisterRequest;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 
 @RestController
