334678ce3f10018e7302075788b69cac3a038fdd
박상혁 <105288887+sangcci@users.noreply.github.com>
2024-11-24 19:56:30 +0900

    [ARV-26] dependency 교체 및 favicon 요청 처리 (#30)
    * [ARV-26] chore: S3 디펜던시 교체 및 spring cloud, spring batch 디펜던시 삭제
    
    * [ARV-26] chore: commons-logging 모듈 제외 및 디펜던시 보기 편하게 수정
    
    * [ARV-26] chore: favicon 요청 시 인증, 인가 로직 미적용
    
    * [ARV-26] chore: 빈 favicon 이미지 반환하도록 수정
    
    * [ARV-26] chore: 가독성을 위한 디펜던시 코드 순서 정리

--- src/main/java/com/backend/allreva/common/application/S3ImageService.java
+++ src/main/java/com/backend/allreva/common/application/S3ImageService.java
@@ -1,13 +1,17 @@
 package com.backend.allreva.common.application;
 
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.backend.allreva.member.command.application.ImageService;
+import io.awspring.cloud.s3.S3Operations;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class S3ImageService implements ImageService {
 
-    private final AmazonS3Client s3Client;
+    @Value("${spring.cloud.aws.s3.bucket:null}")
+    private String bucketName;
+
+    private final S3Operations s3Operations;
 }
