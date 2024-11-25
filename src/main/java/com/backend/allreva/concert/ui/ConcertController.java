b76cfb5243236e8e5837bab35c569c6f973fe7e7
dowoo kim <dowoo12345@naver.com>
2024-11-26 02:30:03 +0900

    feat: ConcertController.getConcertMain

--- src/main/java/com/backend/allreva/concert/ui/ConcertController.java
+++ src/main/java/com/backend/allreva/concert/ui/ConcertController.java
@@ -1,14 +1,17 @@
 package com.backend.allreva.concert.ui;
 
 import com.backend.allreva.common.dto.Response;
+import com.backend.allreva.concert.command.domain.value.SortDirection;
 import com.backend.allreva.concert.query.application.ConcertQueryService;
 import com.backend.allreva.concert.query.application.dto.ConcertDetail;
-
+import com.backend.allreva.concert.query.application.dto.ConcertMainResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
-@RequestMapping("/concerts")
+@RequestMapping("/api/v1/concerts")
 @RestController
 public class ConcertController {
 
@@ -21,5 +24,17 @@ public class ConcertController {
         ConcertDetail detail = concertQueryService.findDetailById(concertId);
         return Response.onSuccess(detail);
     }
+
+    @GetMapping("/list")
+    public Response<ConcertMainResponse> getConcertMain(
+            @RequestParam final String region,
+            @RequestParam(defaultValue = "ASC") final SortDirection sortDirection,
+            @RequestParam final int PageSize,
+            @RequestParam final List<Object> searchAfter
+    ){
+        ConcertMainResponse concertMain = concertQueryService.getConcertMain(region, searchAfter, PageSize, sortDirection);
+        return Response.onSuccess(concertMain);
+    }
+
 
 }
