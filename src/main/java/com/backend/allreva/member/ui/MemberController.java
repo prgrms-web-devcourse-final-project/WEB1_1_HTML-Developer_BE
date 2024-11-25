4cbff46b47d70a7964c4f3fae53d0cc0440764a6
sangcci <pshsh910@gmail.com>
2024-11-25 11:54:02 +0900

    [ARV-32] chore: swagger 추가

--- src/main/java/com/backend/allreva/member/ui/MemberController.java
+++ src/main/java/com/backend/allreva/member/ui/MemberController.java
@@ -4,6 +4,10 @@ import com.backend.allreva.auth.application.AuthMember;
 import com.backend.allreva.member.command.application.MemberCommandService;
 import com.backend.allreva.member.command.application.dto.MemberInfoUpdateRequest;
 import com.backend.allreva.member.command.domain.Member;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -18,6 +22,10 @@ public class MemberController {
 
     private final MemberCommandService memberCommandService;
 
+    @Operation(summary = "회원 프로필 수정", description = "회원 프로필 수정 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "성공적으로 수정했습니다.", content = @Content(mediaType = "application/json"))
+    })
     @PatchMapping
     public ResponseEntity<Void> updateMemberInfo(
             @AuthMember Member member,
--- src/main/java/com/backend/allreva/member/ui/OAuth2RegisterController.java
+++ src/main/java/com/backend/allreva/member/ui/OAuth2RegisterController.java
@@ -1,17 +1,19 @@
 package com.backend.allreva.member.ui;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.backend.allreva.auth.application.AuthMember;
 import com.backend.allreva.member.command.application.MemberCommandService;
 import com.backend.allreva.member.command.application.dto.MemberInfoUpdateRequest;
 import com.backend.allreva.member.command.domain.Member;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,7 +22,11 @@ public class OAuth2RegisterController {
 
     private final MemberCommandService memberCommandService;
 
-    @PatchMapping("/register")
+    @Operation(summary = "oauth2 회원가입", description = "oauth2 회원가입 시 회원 정보 입력 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "성공적으로 가입했습니다.", content = @Content(mediaType = "application/json"))
+    })
+    @PostMapping("/register")
     public ResponseEntity<Void> register(
             @AuthMember Member member,
             @RequestBody MemberInfoUpdateRequest memberInfoUpdateRequest) {
