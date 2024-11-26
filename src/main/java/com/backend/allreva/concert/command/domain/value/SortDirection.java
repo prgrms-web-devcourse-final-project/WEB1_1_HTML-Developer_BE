1d8070925feb86921ee81a6524b91dbdb6d96e44
dowoo kim <dowoo12345@naver.com>
2024-11-26 14:51:46 +0900

    fix :CustomConcertSearchRepo

--- src/main/java/com/backend/allreva/concert/command/domain/value/SortDirection.java
+++ src/main/java/com/backend/allreva/concert/command/domain/value/SortDirection.java
@@ -1,5 +1,5 @@
 package com.backend.allreva.concert.command.domain.value;
 
 public enum SortDirection {
-    ASC, DESC
+    DATE, VIEWS
 }
--- src/main/java/com/backend/allreva/search/infra/CustomConcertSearchRepoImpl.java
+++ src/main/java/com/backend/allreva/search/infra/CustomConcertSearchRepoImpl.java
@@ -6,6 +6,7 @@ import com.backend.allreva.search.query.domain.ConcertDocument;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.client.elc.NativeQuery;
+import org.springframework.data.elasticsearch.client.elc.NativeQueryBuilder;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.util.StringUtils;
@@ -17,37 +18,43 @@ public class CustomConcertSearchRepoImpl implements CustomConcertSearchRepo {
     private final ElasticsearchOperations elasticsearchOperations;
 
     @Override
-    public SearchHits<ConcertDocument> searchMainConcerts(String address, List<Object> searchAfter, int size, SortDirection sortDirection) {
+    public SearchHits<ConcertDocument> searchMainConcerts(final String address, final List<Object> searchAfter, final int size, final SortDirection sortDirection) {
         NativeQuery searchQuery = getNativeQuery(address, searchAfter, size, sortDirection);
         return elasticsearchOperations.search(searchQuery, ConcertDocument.class);
     }
-
-    private static NativeQuery getNativeQuery(String address, List<Object> searchAfter, int size, SortDirection sortDirection) {
-        return NativeQuery.builder()
+    private static NativeQuery getNativeQuery(final String address, final List<Object> searchAfter, final int size, final SortDirection sortDirection) {
+        NativeQueryBuilder nativeQueryBuilder = NativeQuery.builder()
                 .withQuery(q -> {
                     if (StringUtils.hasText(address)) {
                         return q.match(m -> m
-                                .field("concert_hall_address")
+                                .field("concert_hall_address.mixed")
                                 .query(address)
+                                .fuzziness("AUTO")
                         );
                     } else {
-                        return q.matchAll(m -> m.queryName("match_all"));
+                        return q.matchAll(m -> m);
                     }
                 })
                 .withSort(s -> s
                         .field(f -> f
-                                .field(StringUtils.hasText(address) ? "stdate" : "id")
-                                .order(sortDirection == SortDirection.ASC ? SortOrder.Asc : SortOrder.Desc)
+                                .field(sortDirection == SortDirection.DATE
+                                        ? "stdate" : "view_count"
+                                )
+                                .order(SortOrder.Asc)
                         )
                 )
                 .withSort(s -> s
                         .field(f -> f
-                                .field("id")
-                                .order(sortDirection == SortDirection.ASC ? SortOrder.Asc : SortOrder.Desc)
+                                .field("concert_code")
+                                .order(SortOrder.Desc)
                         )
                 )
-                .withPageable(PageRequest.of(0, size + 1))
-                .withSearchAfter(searchAfter)
-                .build();
+                .withPageable(PageRequest.of(0, size));
+
+        if(searchAfter != null && !searchAfter.isEmpty()){
+            nativeQueryBuilder.withSearchAfter(searchAfter);
+        }
+        return nativeQueryBuilder.build();
     }
+
 }
