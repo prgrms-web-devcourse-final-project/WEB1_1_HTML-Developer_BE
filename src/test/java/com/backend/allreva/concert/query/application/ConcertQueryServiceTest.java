c058fedbb9971da39bae376ed6e6f455e0d5d490
dowoo kim <dowoo12345@naver.com>
2024-11-26 15:27:34 +0900

    chore: elasticsearch 테스트 코드 주석처리

--- src/test/java/com/backend/allreva/concert/query/application/ConcertQueryServiceTest.java
+++ src/test/java/com/backend/allreva/concert/query/application/ConcertQueryServiceTest.java
@@ -16,34 +16,34 @@ import static org.hamcrest.Matchers.is;
 
 @Slf4j
 class ConcertQueryServiceTest extends IntegralTestSupport {
-
-    @Autowired
-    ConcertQueryService concertQueryService;
-
-    @Test
-    @DisplayName("콘서트 메인 화면 조회 입력된 지역에 맞춰서 정렬 조건에 맞게 정렬 이후 마지막 searchAfter 보내면 다음 페이지에서 첫 페이지로")
-    void getConcertMain() {
-        List<Object> searchAfter = new ArrayList<>();
-        ConcertMainResponse concertMain = concertQueryService.getConcertMain("서울", searchAfter, 3, SortDirection.VIEWS);
-
-        concertMain.concertMain().forEach(
-                concertMain1 -> log.info("concertMain1: {}", concertMain1)
-        );
-        log.info("concertMain.searchAfter: {}", concertMain.searchAfter());
-
-        ConcertMainResponse concertMain1 = concertQueryService.getConcertMain("서울", concertMain.searchAfter(), 3, SortDirection.VIEWS);
-
-        concertMain1.concertMain().forEach(
-                concertMain2 -> log.info("concertMain2: {}", concertMain2)
-        );
-
-
-
-        ConcertMainResponse concertMain2 = concertQueryService.getConcertMain("서울", searchAfter, 6, SortDirection.VIEWS);
-        concertMain2.concertMain().forEach(
-                concertMain3 -> log.info("concertMain3: {}", concertMain3)
-        );
-
-        assertThat(concertMain.searchAfter().get(0), is(concertMain2.concertMain().get(0).stdate()));
-    }
+//
+//    @Autowired
+//    ConcertQueryService concertQueryService;
+//
+//    @Test
+//    @DisplayName("콘서트 메인 화면 조회 입력된 지역에 맞춰서 정렬 조건에 맞게 정렬 이후 마지막 searchAfter 보내면 다음 페이지에서 첫 페이지로")
+//    void getConcertMain() {
+//        List<Object> searchAfter = new ArrayList<>();
+//        ConcertMainResponse concertMain = concertQueryService.getConcertMain("서울", searchAfter, 3, SortDirection.VIEWS);
+//
+//        concertMain.concertMain().forEach(
+//                concertMain1 -> log.info("concertMain1: {}", concertMain1)
+//        );
+//        log.info("concertMain.searchAfter: {}", concertMain.searchAfter());
+//
+//        ConcertMainResponse concertMain1 = concertQueryService.getConcertMain("서울", concertMain.searchAfter(), 3, SortDirection.VIEWS);
+//
+//        concertMain1.concertMain().forEach(
+//                concertMain2 -> log.info("concertMain2: {}", concertMain2)
+//        );
+//
+//
+//
+//        ConcertMainResponse concertMain2 = concertQueryService.getConcertMain("서울", searchAfter, 6, SortDirection.VIEWS);
+//        concertMain2.concertMain().forEach(
+//                concertMain3 -> log.info("concertMain3: {}", concertMain3)
+//        );
+//
+//        assertThat(concertMain.searchAfter().get(0), is(concertMain2.concertMain().get(0).stdate()));
+//    }
 }
--- src/test/java/com/backend/allreva/search/infra/ConcertRepositoryTest.java
+++ src/test/java/com/backend/allreva/search/infra/ConcertRepositoryTest.java
@@ -1,32 +1,23 @@
 package com.backend.allreva.search.infra;
 
 import com.backend.allreva.IntegralTestSupport;
-import com.backend.allreva.concert.command.domain.ConcertRepository;
-import com.backend.allreva.search.query.domain.ConcertDocument;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 class ConcertRepositoryTest extends IntegralTestSupport{
 
-    @Autowired
-    private ConcertSearchRepository concertSearchRepository;
-
-
-    @Test
-    void test1(){
-        PageRequest pageRequest = PageRequest.of(0, 10);
-
-        List<ConcertDocument> day6 = concertSearchRepository.findByTitleWithFuzziness("day6", pageRequest);
-        System.out.println(day6.size());
-        for (ConcertDocument concertDocument : day6) {
-            log.info("concert: {}", concertDocument.toString());
-        }
-    }
+//    @Autowired
+//    private ConcertSearchRepository concertSearchRepository;
+//
+//
+//    @Test
+//    void test1(){
+//        PageRequest pageRequest = PageRequest.of(0, 10);
+//
+//        List<ConcertDocument> day6 = concertSearchRepository.findByTitleWithFuzziness("day6", pageRequest);
+//        System.out.println(day6.size());
+//        for (ConcertDocument concertDocument : day6) {
+//            log.info("concert: {}", concertDocument.toString());
+//        }
+//    }
 }
