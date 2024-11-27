4f23d0c29d9e694b9baae58854256a4fc8fa4030
sangcci <pshsh910@gmail.com>
2024-11-27 15:22:31 +0900

    [ARV-32] feat: cors 설정 추가, 임시로 disable

--- src/main/java/com/backend/allreva/common/config/SecurityConfig.java
+++ src/main/java/com/backend/allreva/common/config/SecurityConfig.java
@@ -1,5 +1,12 @@
 package com.backend.allreva.common.config;
 
+import com.backend.allreva.auth.exception.CustomAccessDeniedHandler;
+import com.backend.allreva.auth.exception.CustomAuthenticationEntryPoint;
+import com.backend.allreva.auth.filter.JwtAuthenticationFilter;
+import com.backend.allreva.auth.oauth2.application.CustomOAuth2UserService;
+import com.backend.allreva.auth.oauth2.handler.OAuth2LoginFailureHandler;
+import com.backend.allreva.auth.oauth2.handler.OAuth2LoginSuccessHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,74 +24,67 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 
-import com.backend.allreva.auth.exception.CustomAccessDeniedHandler;
-import com.backend.allreva.auth.exception.CustomAuthenticationEntryPoint;
-import com.backend.allreva.auth.filter.JwtAuthenticationFilter;
-import com.backend.allreva.auth.oauth2.application.CustomOAuth2UserService;
-import com.backend.allreva.auth.oauth2.handler.OAuth2LoginFailureHandler;
-import com.backend.allreva.auth.oauth2.handler.OAuth2LoginSuccessHandler;
-
-import lombok.RequiredArgsConstructor;
-
 @Configuration
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-        // 인가 과정에서 허용할 URL 등록
-        private static final String[] ALLOW_URLS = { "/h2-console/**","/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**"};
-        private static final String[] AUTH_URLS = { "/api/v1/oauth2/login/**", "/login/oauth2/**" };
+    // 인가 과정에서 허용할 URL 등록
+    private static final String[] ALLOW_URLS = {"/h2-console/**", "/v3/api-docs/**", "/swagger-ui/**",
+            "/swagger-resources/**"};
+    private static final String[] AUTH_URLS = {"/api/v1/oauth2/login/**", "/login/oauth2/**"};
 
-        // JWT
-        private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
-        private final CustomAccessDeniedHandler customAccessDeniedHandler;
-        private final JwtAuthenticationFilter jwtAuthenticationFilter;
-        // OAuth2
-        private final CustomOAuth2UserService customOAuth2UserService;
-        private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
-        private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
+    // JWT
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    // OAuth2
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+    private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
 
-        @Bean
-        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-                http
-                                .csrf(CsrfConfigurer<HttpSecurity>::disable)
-                                .formLogin(FormLoginConfigurer<HttpSecurity>::disable)
-                                .httpBasic(HttpBasicConfigurer<HttpSecurity>::disable)
-                                .headers(it -> it.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
-                                .sessionManagement(it -> it.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                                .authorizeHttpRequests(authorize -> authorize
-                                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                                                .requestMatchers(AUTH_URLS).permitAll()
-                                                .requestMatchers(ALLOW_URLS).permitAll()
-                                                .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
-                                                .requestMatchers("/api/v1/oauth2/**").hasRole("GUEST")
-                                                .anyRequest().authenticated())
-                                .httpBasic(AbstractHttpConfigurer::disable);
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(CsrfConfigurer<HttpSecurity>::disable)
+                .formLogin(FormLoginConfigurer<HttpSecurity>::disable)
+                .httpBasic(HttpBasicConfigurer<HttpSecurity>::disable)
+                .headers(it -> it.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .sessionManagement(it -> it.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers(AUTH_URLS).permitAll()
+                        .requestMatchers(ALLOW_URLS).permitAll()
+                        .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/v1/oauth2/**").hasRole("GUEST")
+                        .anyRequest().authenticated())
+                .httpBasic(AbstractHttpConfigurer::disable);
 
-                // OAuth2 인증 필터
-                http
-                                .oauth2Login(customConfigurer -> customConfigurer
-                                                .authorizationEndpoint(end -> end.baseUri("/api/v1/oauth2/login"))
-                                                .userInfoEndpoint(endPointConfig -> endPointConfig
-                                                                .userService(customOAuth2UserService))
-                                                .successHandler(oAuth2LoginSuccessHandler)
-                                                .failureHandler(oAuth2LoginFailureHandler));
+        // OAuth2 인증 필터
+        http
+                .oauth2Login(customConfigurer -> customConfigurer
+                        .authorizationEndpoint(end -> end.baseUri("/api/v1/oauth2/login"))
+                        .userInfoEndpoint(endPointConfig -> endPointConfig
+                                .userService(customOAuth2UserService))
+                        .successHandler(oAuth2LoginSuccessHandler)
+                        .failureHandler(oAuth2LoginFailureHandler));
 
-                // jwt 인증 필터
-                http
-                                .addFilterAfter(jwtAuthenticationFilter, LogoutFilter.class);
-                http
-                                .exceptionHandling(exception -> exception
-                                                .authenticationEntryPoint(customAuthenticationEntryPoint))
-                                .exceptionHandling(
-                                                exception -> exception.accessDeniedHandler(customAccessDeniedHandler));
+        // jwt 인증 필터
+        http
+                .addFilterAfter(jwtAuthenticationFilter, LogoutFilter.class);
+        http
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(customAuthenticationEntryPoint))
+                .exceptionHandling(
+                        exception -> exception.accessDeniedHandler(customAccessDeniedHandler));
 
-                return http.build();
-        }
+        return http.build();
+    }
 
-        @Bean
-        public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-                return configuration.getAuthenticationManager();
-        }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 }
