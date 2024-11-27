e90565df85aa78c9dd18b9ef24e6eceaaeb1312f
sangcci <pshsh910@gmail.com>
2024-11-27 11:00:18 +0900

    [ARV-32] test: 회원 가입 테스트 로직 수정

--- src/test/java/com/backend/allreva/member/command/MemberInfoCommandTest.java
+++ src/test/java/com/backend/allreva/member/command/MemberInfoCommandTest.java
@@ -1,14 +1,16 @@
 package com.backend.allreva.member.command;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 import com.backend.allreva.member.command.application.MemberInfoCommandService;
-import com.backend.allreva.member.command.domain.MemberRepository;
 import com.backend.allreva.member.command.application.dto.MemberInfoRequest;
 import com.backend.allreva.member.command.domain.Member;
+import com.backend.allreva.member.command.domain.MemberRepository;
 import com.backend.allreva.member.command.domain.value.LoginProvider;
+import com.backend.allreva.member.command.domain.value.MemberRole;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -52,10 +54,13 @@ public class MemberInfoCommandTest {
         given(memberRepository.save(any(Member.class))).willReturn(member);
 
         // when
-        var updatedMember = memberInfoCommandService.updateMemberInfo(memberInfoRequest, member);
+        var updatedMember = memberInfoCommandService.registerMember(memberInfoRequest, member);
 
         // then
-        assertThat(updatedMember.getMemberInfo().getIntroduce()).isEqualTo("test introduce");
+        assertSoftly(softly -> {
+            softly.assertThat(updatedMember.getMemberInfo().getIntroduce()).isEqualTo("test introduce");
+            softly.assertThat(updatedMember.getMemberRole()).isEqualTo(MemberRole.USER);
+        });
     }
 
     @Test
