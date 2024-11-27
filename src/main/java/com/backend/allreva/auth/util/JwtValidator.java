882feb71a855f59a7c8a123cb4e0bbce9d669013
sangcci <pshsh910@gmail.com>
2024-11-27 16:36:16 +0900

    [ARV-53] fix: CustomException 로깅 처리 추가

--- src/main/java/com/backend/allreva/auth/util/JwtValidator.java
+++ src/main/java/com/backend/allreva/auth/util/JwtValidator.java
@@ -2,6 +2,7 @@ package com.backend.allreva.auth.util;
 
 import javax.crypto.SecretKey;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +17,7 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SignatureException;
 
+@Slf4j
 @Component
 public class JwtValidator {
 
@@ -32,10 +34,13 @@ public class JwtValidator {
                     .build()
                     .parse(token);
         } catch (MalformedJwtException e) {
+            log.error("Invalid JWT token: {}", e.getMessage());
             throw new InvalidJwtTokenException();
         } catch (SignatureException e) {
+            log.error("Invalid JWT token signature: {}", e.getMessage());
             throw new InvalidJwtSignatureException();
         } catch (ExpiredJwtException e) {
+            log.error("Expired JWT token: {}", e.getMessage());
             throw new ExpiredJwtTokenException();
         }
     }
