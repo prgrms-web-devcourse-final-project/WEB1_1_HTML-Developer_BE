af20fb34722a0c38a13e1eba8283bff3d9f868a1
kimdw0823 <103844957+kimdw0823@users.noreply.github.com>
2024-11-28 17:36:11 +0900

    [ARV-60] 검색 페이지 콘서트 검색 api 구현 (#57)
    * refactor : ConcertSearchRepository
    
    * refactor : ConcertSearchRepositoryTest
    
    * merge develop
    
    * feat: ConcertSearchService
    
    * feat: 예외
    
    * feat: SearchController 작성
    
    * feat: test code 작성

--- src/main/java/com/backend/allreva/search/infra/ConcertSearchRepository.java
+++ src/main/java/com/backend/allreva/search/infra/ConcertSearchRepository.java
@@ -1,18 +1,17 @@
 package com.backend.allreva.search.infra;
 
 import com.backend.allreva.search.query.domain.ConcertDocument;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ConcertSearchRepository extends
         ElasticsearchRepository<ConcertDocument, String>,
         CustomConcertSearchRepo {
     @Query("{\"match\": {\"title.mixed\": {\"query\": \"?0\", \"fuzziness\": \"AUTO\"}}}")
-    List<ConcertDocument> findByTitleWithFuzziness(String title, Pageable pageable);
+    Page<ConcertDocument> findByTitleMixed(String title, Pageable pageable);
 
 }
