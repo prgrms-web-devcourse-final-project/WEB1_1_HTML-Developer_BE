21e605c02ea4e589d22ed6cc0e44eeb46eb85619
dowoo kim <dowoo12345@naver.com>
2024-11-29 15:08:24 +0900

    feat: CustomConcertSearchRepoImpl 검색 구현

--- src/main/java/com/backend/allreva/search/infra/CustomConcertSearchRepo.java
+++ src/main/java/com/backend/allreva/search/infra/CustomConcertSearchRepo.java
@@ -1,11 +1,19 @@
 package com.backend.allreva.search.infra;
 
-import com.backend.allreva.concert.command.domain.value.SortDirection;
+import com.backend.allreva.search.query.domain.value.SortDirection;
 import com.backend.allreva.search.query.domain.ConcertDocument;
 import org.springframework.data.elasticsearch.core.SearchHits;
 
 import java.util.List;
 
 public interface CustomConcertSearchRepo {
-    SearchHits<ConcertDocument> searchMainConcerts(String address, List<Object> searchAfter, int size, SortDirection sortDirection);
+    SearchHits<ConcertDocument> searchMainConcerts(
+            String address,
+            List<Object> searchAfter,
+            int size,
+            SortDirection sortDirection);
+    SearchHits<ConcertDocument> searchByTitleList(
+            String query,
+            List<Object> searchAfter,
+            int size);
 }
