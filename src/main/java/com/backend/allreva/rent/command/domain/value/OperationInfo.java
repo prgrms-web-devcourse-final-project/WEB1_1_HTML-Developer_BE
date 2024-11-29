fd06471dd05fe0cfbffff7a75967d5a7ca482301
tnals2384 <tnals2384@gmail.com>
2024-11-29 20:15:44 +0900

    [ARV-83] refactor : RentFormBoardingDate 엔티티 추가

--- src/main/java/com/backend/allreva/rent/command/domain/value/OperationInfo.java
+++ src/main/java/com/backend/allreva/rent/command/domain/value/OperationInfo.java
@@ -1,9 +1,14 @@
 package com.backend.allreva.rent.command.domain.value;
 
+import com.backend.allreva.rent.command.domain.RentFormBoardingDate;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Embedded;
+import jakarta.persistence.OneToMany;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Builder
@@ -14,8 +19,8 @@ public class OperationInfo {
     @Column(nullable = false)
     private String boardingArea;
 
-    @Column(nullable = false)
-    private String boardingDate;
+    @OneToMany(mappedBy = "rentForm")
+    private List<RentFormBoardingDate> boardingDates = new ArrayList<>();
 
     @Column(nullable = false)
     private String upTime;
