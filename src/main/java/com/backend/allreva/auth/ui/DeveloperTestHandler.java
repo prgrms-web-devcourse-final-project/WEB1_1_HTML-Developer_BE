35116fefab12fb0707b378c87b71c3a21ba99e77
sangcci <pshsh910@gmail.com>
2024-12-02 18:01:05 +0900

    [ARV-99] refactor: swagger interface 분리

--- src/main/java/com/backend/allreva/auth/ui/DeveloperTestHandler.java
+++ src/main/java/com/backend/allreva/auth/ui/DeveloperTestHandler.java
@@ -6,15 +6,10 @@ import com.backend.allreva.member.command.domain.Member;
 import com.backend.allreva.member.command.domain.MemberRepository;
 import com.backend.allreva.member.command.domain.value.LoginProvider;
 import com.backend.allreva.member.command.domain.value.MemberRole;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,22 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @Profile("local")
-public class DeveloperTestHandler {
+public class DeveloperTestHandler implements DeveloperTestHandlerSwagger {
 
     private final MemberRepository memberRepository;
 
-    @Operation(summary = "전지전능한 개발자 모드 생성", description = "당신은 해당 API를 호출함으로써 초사이언 개발자 모드를 얻을 수 있습니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json"))
-    })
     @GetMapping("/test-developer")
-    public ResponseEntity<Response<String>> getAdminToken() {
+    public Response<String> getAdminToken() {
         Optional<Member> optionalDeveloper = memberRepository.findMemberByMemberRole(MemberRole.DEVELOPER);
         if (optionalDeveloper.isEmpty()) {
             log.info("GOD saved");
             saveDeveloper();
         }
-        return ResponseEntity.ok(Response.onSuccess("당신은 신이 되었습니다."));
+        return Response.onSuccess("당신은 신이 되었습니다.");
     }
 
     private void saveDeveloper() {
