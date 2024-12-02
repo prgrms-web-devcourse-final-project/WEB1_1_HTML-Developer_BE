e35c7247a5bd60ccced15eb025d085c3483c0068
sangcci <pshsh910@gmail.com>
2024-12-02 11:36:55 +0900

    [ARV-96] refactor: controller interface swagger naming 추가

--- src/main/java/com/backend/allreva/rent/ui/RentController.java
+++ src/main/java/com/backend/allreva/rent/ui/RentController.java
@@ -3,27 +3,51 @@ package com.backend.allreva.rent.ui;
 import com.backend.allreva.auth.application.AuthMember;
 import com.backend.allreva.common.dto.Response;
 import com.backend.allreva.member.command.domain.Member;
+import com.backend.allreva.rent.command.application.RentCommandService;
 import com.backend.allreva.rent.command.application.dto.RentIdRequest;
 import com.backend.allreva.rent.command.application.dto.RentRegisterRequest;
 import com.backend.allreva.rent.command.application.dto.RentUpdateRequest;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "차량 대절 폼 API", description = "차량 대절 폼 API")
-public interface RentController {
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/rents")
+public class RentController implements RentControllerSwagger {
 
-    Response<Long> createRent(
-            @RequestBody RentRegisterRequest rentRegisterRequest,
-            @AuthMember Member member
-    );
+    private final RentCommandService rentCommandService;
 
-    Response<Void> updateRent(
-            @RequestBody RentUpdateRequest rentUpdateRequest,
-            @AuthMember Member member
-    );
+    @PostMapping
+    public Response<Long> createRent(
+            @RequestBody final RentRegisterRequest rentRegisterRequest,
+            @AuthMember final Member member
+    ) {
+        Long rentIdResponse = rentCommandService.registerRent(rentRegisterRequest, member);
 
-    Response<Void> closeRent(
-            @RequestBody RentIdRequest rentIdRequest,
-            @AuthMember Member member
-    );
+        return Response.onSuccess(rentIdResponse);
+    }
+
+    @PatchMapping
+    public Response<Void> updateRent(
+        @RequestBody final RentUpdateRequest rentUpdateRequest,
+        @AuthMember final Member member
+    ) {
+        rentCommandService.updateRent(rentUpdateRequest, member);
+        return Response.onSuccess();
+    }
+
+    @PatchMapping("/close")
+    public Response<Void> closeRent(
+            @RequestBody final RentIdRequest rentIdRequest,
+            @AuthMember final Member member
+    ) {
+        rentCommandService.closeRent(rentIdRequest, member);
+        return Response.onSuccess();
+    }
 }
--- src/main/java/com/backend/allreva/rent/ui/RentViewController.java
+++ src/main/java/com/backend/allreva/rent/ui/RentViewController.java
@@ -2,50 +2,54 @@ package com.backend.allreva.rent.ui;
 
 import com.backend.allreva.common.dto.Response;
 import com.backend.allreva.rent.command.domain.value.Region;
+import com.backend.allreva.rent.query.application.RentQueryService;
 import com.backend.allreva.rent.query.application.dto.DepositAccountResponse;
 import com.backend.allreva.rent.query.application.dto.RentDetailResponse;
 import com.backend.allreva.rent.query.application.dto.RentSummaryResponse;
 import com.backend.allreva.survey.query.application.dto.SortType;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Min;
 import java.time.LocalDate;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "차량 대절 폼 조회 API", description = "차량 대절 폼 조회 API")
-public interface RentViewController {
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/rents")
+public class RentViewController implements RentViewControllerSwagger {
 
-    @Operation(
-            summary = "차량 대절 폼 상세 조회 API",
-            description = """
-                    차량 대절 폼을 상세 조회합니다.
-                    boardingDate 는 2024.11.30(토) 와 같은 형태로 반환됩니다.
-                    endDate는 2024-11-30 과 같은 형태로 반환됩니다.
-                    """)
-    Response<RentDetailResponse> getRentDetailById(
-            Long id
-    );
+    private final RentQueryService rentQueryService;
 
-    @Operation(
-            summary = "입금 계좌 조회 API",
-            description = """
-                    입금 계좌를 조회합니다.
-                    현재 사용자가 USER 권한보다 아래면 입금 계좌를 조회할 수 없습니다.
-                    """)
-    Response<DepositAccountResponse> getDepositAccountById(
-            Long id
-    );
+    @GetMapping("/{id}")
+    public Response<RentDetailResponse> getRentDetailById(
+            @PathVariable final Long id
+    ) {
+        return Response.onSuccess(rentQueryService.getRentDetailById(id));
+    }
 
-    @Operation(
-            summary = "차량 대절 폼 리스트 조회 API",
-            description = """
-                    차량 대절 폼의 요약된 정보를 리스트로 조회합니다.
-                    """)
-    Response<List<RentSummaryResponse>> getRentSummaries(
-            Region region,
-            SortType sortType,
-            Long lastId,
-            LocalDate lastEndDate,
-            @Min(10) int pageSize
-    );
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/{id}/deposit-account")
+    public Response<DepositAccountResponse> getDepositAccountById(
+            @PathVariable final Long id
+    ) {
+        return Response.onSuccess(rentQueryService.getDepositAccountById(id));
+    }
+
+    @GetMapping("/list")
+    public Response<List<RentSummaryResponse>> getRentSummaries(
+            @RequestParam(name = "region", required = false) final Region region,
+            @RequestParam(name = "sort", defaultValue = "LATEST") final SortType sortType,
+            @RequestParam(name = "lastId", required = false) final Long lastId,
+            @RequestParam(name = "lastEndDate", required = false) final LocalDate lastEndDate,
+            @RequestParam(name = "pageSize", defaultValue = "10") @Min(10) final int pageSize
+    ) {
+        return Response.onSuccess(rentQueryService.getRentSummaries(region, sortType, lastEndDate, lastId, pageSize));
+    }
 }
