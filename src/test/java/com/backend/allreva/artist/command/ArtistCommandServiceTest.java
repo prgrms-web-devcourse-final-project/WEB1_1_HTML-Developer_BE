ecca67c99b09a885db0fb0f786d8bf75365e0b13
dowoo kim <dowoo12345@naver.com>
2024-12-03 15:02:11 +0900

    [ARV-101] feat: test 코드 작성

---
+++ src/test/java/com/backend/allreva/artist/command/ArtistCommandServiceTest.java
@@ -0,0 +1,44 @@
+package com.backend.allreva.artist.command;
+
+import com.backend.allreva.artist.query.application.ArtistQueryService;
+import com.backend.allreva.artist.query.application.dto.SpotifySearchResponse;
+import com.backend.allreva.member.command.application.dto.MemberInfoRequest.MemberArtistRequest;
+import com.backend.allreva.support.IntegrationTestSupport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ArtistCommandServiceTest extends IntegrationTestSupport {
+    @Autowired
+    ArtistCommandService artistCommandService;
+
+    @Autowired
+    ArtistQueryService artistQueryService;
+
+    @DisplayName("아티스트 검색후 값이 존재하지 않을 경우 DB에 삽입")
+    @Test
+    void saveIfNotExistTest() {
+        //given
+        String query = "day6";
+        List<SpotifySearchResponse> responses = artistQueryService.searchArtist(query);
+        List<MemberArtistRequest> memberArtistRequests = responses.stream()
+                .map(response -> {
+                    return new MemberArtistRequest(response.id(), response.name());
+                }).toList();
+
+        //when
+        artistCommandService.saveIfNotExist(memberArtistRequests);
+
+        //then
+        memberArtistRequests.forEach(memberArtistRequest -> {
+            assertThat(
+                    artistCommandService.getArtistById(memberArtistRequest.spotifyArtistId())
+            ).isNotNull();
+        });
+    }
+
+}
