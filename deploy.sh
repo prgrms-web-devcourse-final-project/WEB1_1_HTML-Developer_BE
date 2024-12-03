982f907772ea27775a2bd2928de3db66047aa489
tnals2384 <tnals2384@gmail.com>
2024-12-03 16:20:24 +0900

    [ARV-104] chore : docker-compose.yml 수정

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@
 
 IS_GREEN=$(docker ps | grep green) # 현재 실행중인 App이 blue인지 확인합니다.
 
-if [ -z $IS_GREEN  ];then # blue라면
+if [ -z "$IS_GREEN" ]; then # green이 없으면 blue라면
 
   echo "### BLUE => GREEN ###"
 
   echo "1. get green image"
-  docker compose pull green # green으로 이미지를 내려받습니다.
+  docker-compose pull green # green으로 이미지를 내려받습니다.
 
   echo "2. green container up"
-  docker compose up -d green # green 컨테이너 실행
+  docker-compose up -d green # green 컨테이너 실행
 
   for cnt in {1..10}
   do
@@ -18,20 +18,19 @@ if [ -z $IS_GREEN  ];then # blue라면
     echo "서버 응답 확인중(${cnt}/10)";
 
     REQUEST=$(curl http://127.0.0.1:8080) # green으로 request
-      if [ -n "$REQUEST" ]
-        then # 서비스 가능하면 health check 중지
-          echo "health check success"
-          break ;
-        else
-          sleep 10
-
-      fi
+    if [ -n "$REQUEST" ]
+    then # 서비스 가능하면 health check 중지
+      echo "health check success"
+      break ;
+    else
+      sleep 10
+    fi
   done;
 
   if [ $cnt -eq 10 ]
   then
-  	echo "서버가 정상적으로 구동되지 않았습니다."
-  	exit 1
+    echo "서버가 정상적으로 구동되지 않았습니다."
+    exit 1
   fi
 
   echo "4. reload nginx"
@@ -39,15 +38,15 @@ if [ -z $IS_GREEN  ];then # blue라면
   sudo nginx -s reload
 
   echo "5. blue container down"
-  docker compose stop blue
+  docker-compose stop blue
 else
   echo "### GREEN => BLUE ###"
 
   echo "1. get blue image"
-  docker compose pull blue
+  docker-compose pull blue
 
   echo "2. blue container up"
-  docker compose up -d blue
+  docker-compose up -d blue
 
   for cnt in {1..10}
   do
@@ -57,19 +56,18 @@ else
     REQUEST=$(curl http://127.0.0.1:8081) # blue로 request
 
     if [ -n "$REQUEST" ]
-      then # 서비스 가능하면 health check 중지
-        echo "health check success"
-        break ;
-      else
-        sleep 10
-
+    then # 서비스 가능하면 health check 중지
+      echo "health check success"
+      break ;
+    else
+      sleep 10
     fi
   done;
 
   if [ $cnt -eq 10 ]
   then
-  	echo "서버가 정상적으로 구동되지 않았습니다."
-  	exit 1
+    echo "서버가 정상적으로 구동되지 않았습니다."
+    exit 1
   fi
 
   echo "4. reload nginx"
@@ -77,5 +75,5 @@ else
   sudo nginx -s reload
 
   echo "5. green container down"
-  docker compose stop green
+  docker-compose stop green
 fi
