5db8197c1e67449a4e51f78c878648fd2cea20d6
kwanse <khan67781397@gmail.com>
2024-12-04 14:03:39 +0900

    [ARV-91] fix: 테스트 오류 제거

--- src/test/java/com/backend/allreva/concert/ConcertQueryTest.java
+++ src/test/java/com/backend/allreva/concert/ConcertQueryTest.java
@@ -5,6 +5,7 @@ import com.backend.allreva.common.model.Image;
 import com.backend.allreva.concert.command.domain.Concert;
 import com.backend.allreva.concert.command.domain.ConcertRepository;
 import com.backend.allreva.concert.command.domain.value.*;
+import com.backend.allreva.concert.infra.ConcertJpaRepository;
 import com.backend.allreva.concert.query.application.ConcertQueryService;
 import com.backend.allreva.concert.query.application.dto.ConcertDetailResponse;
 import com.backend.allreva.hall.command.domain.ConcertHall;
@@ -29,6 +30,8 @@ class ConcertQueryTest extends IntegrationTestSupport {
 
     @Autowired
     private ConcertRepository concertRepository;
+    @Autowired
+    private ConcertJpaRepository concertJpaRepository;
     @Autowired
     private ConcertHallRepository concertHallRepository;
 
