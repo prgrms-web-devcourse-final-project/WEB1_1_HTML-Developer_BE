9cf0e76ee26143527b6a1caa5f0d4fe5fbe0a7e6
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-12-06 16:49:06 +0900

    [ARV-136] fix : swagger config server 추가 (#117)

--- src/main/java/com/backend/allreva/common/config/SwaggerConfig.java
+++ src/main/java/com/backend/allreva/common/config/SwaggerConfig.java
@@ -5,9 +5,12 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -17,12 +20,21 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         SecurityRequirement developerRequirement = new SecurityRequirement().addList("USER");
 
+
+        Server server1 = new Server();
+        server1.setUrl("https://allreva.shop");
+
+        Server server2 = new Server();
+        server2.setUrl("http://localhost:8080");
+
         return new OpenAPI()
                 .info(new Info().title("AllReva"))
+                .servers(List.of(server1, server2))
                 .components(new Components()
                         .addSecuritySchemes("Auth", createSecurityScheme()))
                 .addSecurityItem(developerRequirement);
     }
+
 
     private SecurityScheme createSecurityScheme() {
         return new SecurityScheme()
