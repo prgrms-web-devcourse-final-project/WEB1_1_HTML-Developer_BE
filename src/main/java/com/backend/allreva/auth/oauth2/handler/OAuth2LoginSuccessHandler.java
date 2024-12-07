1cd9f620116f45bac395be6eb80c5b8bdc613c66
sangcci <pshsh910@gmail.com>
2024-12-07 18:33:18 +0900

    hotfix: oauth2 로그인 성공 시 리다이렉트 URL에 access token 발급

--- src/main/java/com/backend/allreva/auth/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/backend/allreva/auth/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -10,13 +10,14 @@ import com.backend.allreva.member.command.domain.value.MemberRole;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
@@ -63,17 +64,18 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
         CookieUtil.addCookie(response, "accessToken", accessToken, ACCESS_TIME);
         CookieUtil.addCookie(response, "refreshToken", refreshToken, REFRESH_TIME);
 
-        sendRedirect(response, member);
+        sendRedirect(response, member, accessToken);
     }
 
     private void sendRedirect(
             final HttpServletResponse response,
-            final Member member
+            final Member member,
+            final String accessToken
     ) throws IOException {
         if (member.getMemberRole().equals(MemberRole.USER)) {
-            response.sendRedirect(FRONT_BASE_URL);
+            response.sendRedirect(FRONT_BASE_URL + "/auth-success?accessToken=" + URLEncoder.encode(accessToken, StandardCharsets.UTF_8));
         } else {
-            response.sendRedirect(FRONT_BASE_URL + FRONT_SIGNUP_URL);
+            response.sendRedirect(FRONT_BASE_URL + FRONT_SIGNUP_URL + "/auth-success?accessToken=" + URLEncoder.encode(accessToken, StandardCharsets.UTF_8));
         }
     }
 }
