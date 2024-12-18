d87a0164dfffd75ddf16b5af651e8ca928b4c31d
sangcci <pshsh910@gmail.com>
2024-12-18 23:18:34 +0900

    hotfix: allowCredentials true를 위해 allowedOriginPatterns url 지정

--- src/main/java/com/backend/allreva/common/config/WebMvcConfig.java
+++ src/main/java/com/backend/allreva/common/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.backend.allreva.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Value("${url.front.protocol}")
+    private String frontProtocol;
+    @Value("${url.front.domain}")
+    private String frontDomain;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns("http://localhost:3000", frontProtocol + "://" + frontDomain)
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
