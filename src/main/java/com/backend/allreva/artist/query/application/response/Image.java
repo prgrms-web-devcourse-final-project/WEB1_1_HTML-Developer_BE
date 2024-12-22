15560822d997a66617ce4394c3eeb9c3d31b8528
dowoo kim <dowoo12345@naver.com>
2024-12-22 13:57:04 +0900

    [ARV-165] feat 아티스트 검색시 이미지 추가

--- src/main/java/com/backend/allreva/artist/query/application/response/Image.java
+++ src/main/java/com/backend/allreva/artist/query/application/response/Image.java
@@ -2,11 +2,10 @@ package com.backend.allreva.artist.query.application.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.util.List;
-
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record SpotifySearchResponse(
-        String id,
-        String name,
-        List<Image> images
-) {}
+public record Image(
+        String url,
+        int height,
+        int width
+) {
+}
