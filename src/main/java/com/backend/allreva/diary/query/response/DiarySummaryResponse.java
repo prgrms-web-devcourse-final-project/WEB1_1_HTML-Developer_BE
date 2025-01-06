29a7e34440582837d4f9ec41138e1e3c00ea0769
kwanse <129512238+kwanse@users.noreply.github.com>
2025-01-06 21:13:13 +0900

    [ARV-186] diary 수정 (#167)
    * [ARV-186] fix: rent 버그 수정
    
    * [ARV-186] fix: diary 버그 수정

--- src/main/java/com/backend/allreva/diary/query/response/DiarySummaryResponse.java
+++ src/main/java/com/backend/allreva/diary/query/response/DiarySummaryResponse.java
@@ -5,6 +5,7 @@ import com.backend.allreva.common.model.Image;
 import java.time.LocalDate;
 
 public record DiarySummaryResponse(
+        Long diaryId,
         Image concertPoster,
         LocalDate date
 ) {
