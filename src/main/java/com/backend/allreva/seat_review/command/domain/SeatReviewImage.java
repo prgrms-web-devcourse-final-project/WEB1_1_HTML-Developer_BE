ffc110b2fc7169ce47d3521513317d8a1fcb50bd
dowoo kim <dowoo12345@naver.com>
2025-01-12 13:55:31 +0900

    [ARV-203] refactor: SeatReview 도메인 수정

--- src/main/java/com/backend/allreva/seat_review/command/domain/SeatReviewImage.java
+++ src/main/java/com/backend/allreva/seat_review/command/domain/SeatReviewImage.java
@@ -3,37 +3,22 @@ package com.backend.allreva.seat_review.command.domain;
 import com.backend.allreva.common.model.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
-import org.hibernate.validator.constraints.Range;
 
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @SQLRestriction("deleted_at IS NULL")
 @SQLDelete(sql = "UPDATE seat_review SET deleted_at = NOW() WHERE id = ?")
-public class SeatReview extends BaseEntity {
+public class SeatReviewImage extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
-    private String seat;
-
-    @Column(nullable = true)
-    private String content;
+    private String url;
 
     @Column(nullable = false)
-    @Range(min = 0, max = 5)
-    private int star;
-
-    @Column(nullable = false)
-    private Long memberId;
-
-    @Column(nullable = false)
-    private String hallId;
+    private Long seatReviewId;
 }
